9aacba2bc81547b4a38c789acd00c7c92b9ab88c
chichikolon <djimgoupat@yahoo.fr>
2026-02-28 01:38:40 +0100

    UI-Redesign und Vision-DLL-Interop für Objekterkennung
    Die Benutzeroberfläche wurde grundlegend überarbeitet: Neues zentriertes Layout mit Ergebnisanzeige und Start/Stop-Buttons, Fenstertitel und -größe angepasst. In MainWindow.xaml.cs wurde die Anbindung an eine externe Vision-DLL implementiert, inklusive Timer-gesteuerter Abfrage und Anzeige der Erkennungsergebnisse. Die neue Datei VisionInterop.cs kapselt die Interop-Funktionen zur Kommunikation mit der nativen DLL "NeuroC_ComVision.dll" für Kamera-Start, Frame-Abfrage und Kamera-Stopp.

--- VisionClientWPF/MainWindow.xaml.cs
+++ VisionClientWPF/MainWindow.xaml.cs
@@ -1,24 +1,53 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace VisionClientWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _timer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(100);
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (VisionInterop.StartCamera())
+            {
+                _timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Kamera konnte nicht gestartet werden.");
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            VisionInterop.StopCamera();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (VisionInterop.GetFrame(out VisionInterop.DetectionResult result))
+            {
+                if (result.detected)
+                {
+                    PositionText.Text = $"X: {result.x}  Y: {result.y}";
+                }
+                else
+                {
+                    PositionText.Text = "Kein Objekt erkannt";
+                }
+            }
         }
     }
 }
---
+++ VisionClientWPF/VisionInterop.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VisionClientWPF
+{
+    public static class VisionInterop
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DetectionResult
+        {
+            public int x;
+            public int y;
+            public int width;
+            public int height;
+            public bool detected;
+        }
+
+        [DllImport("NeuroC_ComVision.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool StartCamera();
+
+        [DllImport("NeuroC_ComVision.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool GetFrame(out DetectionResult result);
+
+        [DllImport("NeuroC_ComVision.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void StopCamera();
+    }
+}
